543b3761e5080fed248458c6d66226fc1fad12f4
Null <2584456014@qq.com>
2021-04-27 20:23:52 +0800

    update

--- index.py
+++ index.py
@@ -36,72 +36,84 @@ REPOS_JSON_PATH = os.environ['REPOS_JSON_PATH']
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return 'OK'
-    elif request.method == 'POST':
-        # Store the IP address of the requester
-        request_ip = ipaddress.ip_address(u'{0}'.format(request.remote_addr))
 
-        # If VALIDATE_SOURCEIP is set to false, do not validate source IP
-        if os.environ.get('VALIDATE_SOURCEIP', None) != 'false':
+    try:
+        if request.method == 'GET':
+            return 'test ok !'
 
-            # If GHE_ADDRESS is specified, use it as the hook_blocks.
-            if os.environ.get('GHE_ADDRESS', None):
-                hook_blocks = [unicode(os.environ.get('GHE_ADDRESS'))]
-            # Otherwise get the hook address blocks from the API.
-            else:
-                hook_blocks = requests.get('https://api.github.com/meta').json()[
-                    'hooks']
+        elif request.method == 'POST':
+            # Store the IP address of the requester
+            request_ip = ipaddress.ip_address(u'{0}'.format(request.remote_addr))
 
-            # Check if the POST request is from github.com or GHE
-            for block in hook_blocks:
-                if ipaddress.ip_address(request_ip) in ipaddress.ip_network(block):
-                    break  # the remote_addr is within the network range of github.
-            else:
-                if str(request_ip) != '127.0.0.1':
-                    abort(403)
+            # If VALIDATE_SOURCEIP is set to false, do not validate source IP
+            if os.environ.get('VALIDATE_SOURCEIP', None) != 'false':
 
-        if request.headers.get('X-GitHub-Event') == "ping":
-            return json.dumps({'msg': 'Hi!'})
-        if request.headers.get('X-GitHub-Event') != "push":
-            return json.dumps({'msg': "wrong event type"})
+                # If GHE_ADDRESS is specified, use it as the hook_blocks.
+                if os.environ.get('GHE_ADDRESS', None):
+                    hook_blocks = [unicode(os.environ.get('GHE_ADDRESS'))]
+                # Otherwise get the hook address blocks from the API.
+                else:
+                    hook_blocks = requests.get('https://api.github.com/meta').json()['hooks']
 
-        repos = json.loads(io.open(REPOS_JSON_PATH, 'r').read())
+                # Check if the POST request is from github.com or GHE
+                for block in hook_blocks:
+                    if ipaddress.ip_address(request_ip) in ipaddress.ip_network(block):
+                        break  # the remote_addr is within the network range of github.
+                else:
+                    if str(request_ip) != '127.0.0.1':
+                        abort(403)
 
-        payload = json.loads(request.data)
-        repo_meta = {
-            'name': payload['repository']['name'],
-            'owner': payload['repository']['owner']['name'],
-        }
+            if request.headers.get('X-GitHub-Event') == "ping":
+                return json.dumps({'msg': 'Hi!'})
+            if request.headers.get('X-GitHub-Event') != "push":
+                return json.dumps({'msg': "wrong event type"})
 
-        # Try to match on branch as configured in repos.json
-        match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
-        if match:
-            repo_meta['branch'] = match.groupdict()['branch']
-            repo = repos.get(
-                '{owner}/{name}/branch:{branch}'.format(**repo_meta), None)
+            repos = json.loads(io.open(REPOS_JSON_PATH, 'r').read())
 
-            # Fallback to plain owner/name lookup
+            payload = json.loads(request.data)
+            repo_meta = {
+                'name': payload['repository']['name'],
+                'owner': payload['repository']['owner']['name'],
+            }
+
+            # Try to match on branch as configured in repos.json
+            match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
+            if match:
+                repo_meta['branch'] = match.groupdict()['branch']
+                repo = repos.get(
+                    '{owner}/{name}/branch:{branch}'.format(**repo_meta), None)
+
+                # Fallback to plain owner/name lookup
+                if not repo:
+                    repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
+
             if not repo:
-                repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
+                return 'not found target repo !'
 
-        if repo and repo.get('path', None):
-            # Check if POST request signature is valid
-            key = repo.get('key', None)
-            if key:
-                signature = request.headers.get('X-Hub-Signature').split(
-                    '=')[1]
-                if type(key) == unicode:
-                    key = key.encode()
-                mac = hmac.new(key, msg=request.data, digestmod=sha1)
-                if not compare_digest(mac.hexdigest(), signature):
-                    abort(403)
+            if repo.get('path', None):
+                # Check if POST request signature is valid
+                key = repo.get('key', None)
+                if key:
+                    signature = request.headers.get('X-Hub-Signature').split(
+                        '=')[1]
+                    if type(key) == unicode:
+                        key = key.encode()
+                    mac = hmac.new(key, msg=request.data, digestmod=sha1)
+                    if not compare_digest(mac.hexdigest(), signature):
+                        abort(403)
 
-        if repo.get('action', None):
-            for action in repo['action']:
-                subp = subprocess.Popen(action, cwd=repo.get('path', '.'))
-                subp.wait()
-        return 'OK'
+            if repo.get('action', None):
+                for action in repo['action']:
+                    subp = subprocess.Popen(action, cwd=repo.get('path', '.'))
+                    subp.wait()
+
+            return 'done !'
+
+    # goto error
+    except Exception as err:
+
+        return 'error: {0}'.format(err)
+    
 
 # Check if python version is less than 2.7.7
 if sys.version_info < (2, 7, 7):
@@ -128,10 +140,13 @@ else:
     compare_digest = hmac.compare_digest
 
 if __name__ == "__main__":
+    
     try:
         port_number = int(sys.argv[1])
     except:
         port_number = 80
+
     if os.environ.get('USE_PROXYFIX', None) == 'true':
         app.wsgi_app = ProxyFix(app.wsgi_app)
+
     app.run(host='0.0.0.0', port=port_number)
